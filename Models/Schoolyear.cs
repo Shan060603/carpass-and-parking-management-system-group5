223ec53bb79269449f5989813a8ecdb14035a5fc
Shan Marion Silveo <shan.silveo@gmail.com>
2025-06-24 01:08:48 +0800

    Update Schoolyear.cs

--- Models/Schoolyear.cs
+++ Models/Schoolyear.cs
@@ -3,13 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Carpass_Profilling.Models
 {
-    [Table("syear")] // Must match your actual MySQL table name
+    [Table("syear")] // Matches your table name in the database
     public class Schoolyear
     {
         [Key]
         public int Sy_ID { get; set; }
 
         [Column("Year")]
-        public string Year { get; set; }
+        [Required]
+        public string Year { get; set; } = string.Empty;
     }
 }
