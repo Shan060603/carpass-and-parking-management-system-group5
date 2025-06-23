fbe2a1a2109454edec883ac4b3c32ee0ddcbb8e7
Shan Marion Silveo <shan.silveo@gmail.com>
2025-06-24 00:45:27 +0800

    Update Applicant.cs

--- Models/Applicant.cs
+++ Models/Applicant.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // ✅ ADD THIS
+
 
 namespace Carpass_Profilling.Models
 {
