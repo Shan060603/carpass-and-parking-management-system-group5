232cd84ef7d5a1b1759712a68a20ec9ad275e3c6
Shan Marion Silveo <shan.silveo@gmail.com>
2025-06-23 23:44:12 +0800

    Update DataContext.cs

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -17,6 +17,8 @@ namespace Carpass_Profilling.Data
         public DbSet<Pending> Pendings { get; set; }
         public DbSet<Central_Data> Central_Datas { get; set; }
         public DbSet<Schoolyear> Syear { get; set; }
+        public DbSet<User> Users { get; set; }
+
 
         
 
